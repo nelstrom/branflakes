03e4de838d8e2e89aad610d37a10b205dc72a5b8
Drew Neil <andrew.jr.neil@gmail.com>
2023-08-21 15:57:54 +0100

    Enable embroider

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,5 +7,16 @@ module.exports = function (defaults) {
     // Add options here
   });
 
-  return app.toTree();
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticHelpers: true,
+    staticModifiers: true,
+    staticComponents: true,
+    splitAtRoutes: ['one', 'two', 'three'], // can also be a RegExp
+    packagerOptions: {
+      webpackConfig: {},
+    },
+  });
 };
