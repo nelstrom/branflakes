28d269b36fad320785a4aee83eb48dd3db26494d
Drew Neil <andrew.jr.neil@gmail.com>
2023-08-22 08:50:24 +0100

    Use dynamic import for highlight.js

--- app/modifiers/highlighter.js
+++ app/modifiers/highlighter.js
@@ -1,7 +1,9 @@
 import { modifier } from 'ember-modifier';
-import hljs from 'highlight.js';
 import 'highlight.js/styles/github.css';
 
-export default modifier(function highlighter(element) {
+export default modifier(async function highlighter(element) {
+  const module = await import('highlight.js');
+  const hljs = module.default;
+
   hljs.highlightElement(element);
 });
