23643227145516e0604385dd9880c667401947e6
Drew Neil <andrew.jr.neil@gmail.com>
2023-08-25 16:06:25 +0100

    Simplify list-sort

--- app/utils/list-sort.js
+++ app/utils/list-sort.js
@@ -1,27 +1,15 @@
-function compareNumbers(a, b) {
-  return a - b;
-}
-
-function compareStrings(a, b) {
+function compare(a, b) {
   if (a < b) {
     return -1;
   }
   if (a > b) {
     return 1;
   }
-
-  // names must be equal
   return 0;
 }
 
 export function listSort(original, field) {
   return [...original].sort((a, b) => {
-    if (field === 'position') {
-      return compareNumbers(a[field], b[field]);
-    } else if (field === 'name') {
-      return compareStrings(a[field], b[field]);
-    } else {
-      return 0;
-    }
+    return compare(a[field], b[field]);
   });
 }
