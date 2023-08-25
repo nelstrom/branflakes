8be8c21dd3c75538a086e215758357b3abb9d325
Drew Neil <andrew.jr.neil@gmail.com>
2023-08-25 16:13:41 +0100

    "Ordering test" for illustrating randomize checkbox

---
+++ tests/unit/ordering-test.js
@@ -0,0 +1,25 @@
+import { module, test } from 'qunit';
+
+module('Ordering test', function () {
+  test('do', function (assert) {
+    assert.ok('do', 'a deer, a female deer');
+  });
+  test('re', function (assert) {
+    assert.ok('re', 'a drop of golden sun');
+  });
+  test('mi', function (assert) {
+    assert.ok('mi', 'a name I call myself');
+  });
+  test('fa', function (assert) {
+    assert.ok('fa', 'a long long way to run');
+  });
+  test('so', function (assert) {
+    assert.ok('so', 'a needle pulling thread');
+  });
+  test('la', function (assert) {
+    assert.ok('la', 'a note to follow so');
+  });
+  test('ti', function (assert) {
+    assert.ok('ti', 'a drink with jam and bread');
+  });
+});
