273071cd4aac96152f772583a243fddccaede050
Drew Neil <andrew.jr.neil@gmail.com>
2023-08-25 10:32:34 +0100

    Rewrite test

--- tests/unit/naive-list-sort-test.js
+++ tests/unit/naive-list-sort-test.js
@@ -24,16 +24,28 @@ module('List sort (naive)', function () {
   test('sort by name', function (assert) {
     const sorted = listSort(original, 'name');
     assert.deepEqual(
-      sorted.map((item) => item.name),
-      ['apple', 'bear', 'orange', 'pear']
+      sorted.map((item) => [item.position, item.name]),
+      [
+        [1, 'apple'],
+        [2, 'bear'],
+        [3, 'orange'],
+        [4, 'pear'],
+      ],
+      'List is sorted alphabetically by "name" field'
     );
   });
 
   test('sort by position', function (assert) {
     const sorted = listSort(original, 'position');
     assert.deepEqual(
-      sorted.map((item) => item.position),
-      [1, 2, 3, 4]
+      sorted.map((item) => [item.position, item.name]),
+      [
+        [1, 'apple'],
+        [2, 'bear'],
+        [3, 'orange'],
+        [4, 'pear'],
+      ],
+      'List is sorted numerically by "position" field'
     );
   });
 });
