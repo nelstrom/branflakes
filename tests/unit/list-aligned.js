0d337056e07539ec6023643b00f1a33ca8607dcb
Drew Neil <andrew.jr.neil@gmail.com>
2023-08-25 10:32:49 +0100

    Add aligned/askew lists

---
+++ tests/unit/list-aligned.js
@@ -0,0 +1,18 @@
+export const original = [
+  {
+    name: 'apple',
+    position: 1,
+  },
+  {
+    name: 'bear',
+    position: 2,
+  },
+  {
+    name: 'orange',
+    position: 3,
+  },
+  {
+    name: 'pear',
+    position: 4,
+  },
+];
---
+++ tests/unit/list-askew.js
@@ -0,0 +1,18 @@
+export const original = [
+  {
+    name: 'orange',
+    position: 1,
+  },
+  {
+    name: 'pear',
+    position: 4,
+  },
+  {
+    name: 'apple',
+    position: 3,
+  },
+  {
+    name: 'bear',
+    position: 2,
+  },
+];
