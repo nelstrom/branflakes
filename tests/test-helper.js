f94ec874a0e7be00efdc1ef23497a14614ec21ac
Drew Neil <andrew.jr.neil@gmail.com>
2023-08-28 20:49:21 +0100

    Log the Qunit seed with link to repeat

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -15,4 +15,13 @@ QUnit.config.urlConfig.push({
   tooltip: 'When checked, tests run in a random order',
 });
 
+if (QUnit.config.seed) {
+  const { origin, pathname, search } = window.location;
+  const params = new URLSearchParams(search);
+  params.set('seed', QUnit.config.seed);
+  const link = new URL(`${origin}${pathname}?${params}`);
+  console.log('Using seed: ', QUnit.config.seed);
+  console.log('Click link to re-run tests with same seed: ', link.href);
+}
+
 start();
