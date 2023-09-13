6a3e10223ba4996b4d2b6c05321ecfe29bd1598a
Drew Neil <drew.neil@crowdstrike.com>
2023-09-13 06:24:31 +0100

    Fiddle with global state tests

--- tests/acceptance/global-variable-test.js
+++ tests/acceptance/global-variable-test.js
@@ -1,21 +1,21 @@
 import { visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'branflakes/tests/helpers';
 import { module, test } from 'qunit';
-import { setupBrowserFakes } from 'ember-browser-services/test-support';
+// import { setupBrowserFakes } from 'ember-browser-services/test-support';
 
 module('Acceptance | global variable', function (hooks) {
   setupApplicationTest(hooks);
-  setupBrowserFakes(hooks, { window: true });
+  // setupBrowserFakes(hooks, { window: true });
 
   test('Visit route one', async function (assert) {
-    let window = this.owner.lookup('service:browser/window');
+    // let window = this.owner.lookup('service:browser/window');
 
     await visit('/one');
     assert.strictEqual(window.myGlobalVariable, 'Hello!');
   });
 
   test('Visit route two', async function (assert) {
-    let window = this.owner.lookup('service:browser/window');
+    // let window = this.owner.lookup('service:browser/window');
 
     await visit('/two');
     assert.strictEqual(window.myGlobalVariable, 'Hi!');
@@ -23,7 +23,7 @@ module('Acceptance | global variable', function (hooks) {
 
   test('Visit route three', async function (assert) {
     await visit('/three');
-    let window = this.owner.lookup('service:browser/window');
+    // let window = this.owner.lookup('service:browser/window');
 
     // This test fails when it runs after 'Visit route two'
     assert.strictEqual(window.myGlobalVariable, 'Hello!');
