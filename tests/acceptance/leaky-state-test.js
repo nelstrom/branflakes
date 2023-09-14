949865cbfaa6037b8ae0bdf8ebe0b738fc5f798a
Drew Neil <drew.neil@crowdstrike.com>
2023-09-14 21:22:28 +0100

    Fix table

---
+++ tests/acceptance/leaky-state-test.js
@@ -0,0 +1,22 @@
+import { visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'branflakes/tests/helpers';
+import { module, test } from 'qunit';
+
+module('Acceptance | leaky state', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('Visit route one', async function (assert) {
+    await visit('/one');
+    assert.strictEqual(window.myGlobalVariable, 'Uno');
+  });
+
+  test('Visit route two', async function (assert) {
+    await visit('/two');
+    assert.strictEqual(window.myGlobalVariable, 'Dos');
+  });
+
+  test('Visit route three', async function (assert) {
+    await visit('/three');
+    assert.strictEqual(window.myGlobalVariable, undefined);
+  });
+});
