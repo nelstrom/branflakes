7d8259a3ca3a3f1cc9a454d6e2062610455db10a
Drew Neil <drew.neil@crowdstrike.com>
2023-09-20 18:45:45 +0200

    Remove ember-browser-services

--- app/routes/one.js
+++ app/routes/one.js
@@ -1,10 +1,7 @@
 import Route from '@ember/routing/route';
-import { service } from '@ember/service';
 
 export default class OneRoute extends Route {
-  @service('browser/window') window;
-
   model() {
-    this.window.myGlobalVariable = 'Uno';
+    window.myGlobalVariable = 'Uno';
   }
 }
--- app/routes/two.js
+++ app/routes/two.js
@@ -1,10 +1,7 @@
 import Route from '@ember/routing/route';
-import { service } from '@ember/service';
 
 export default class TwoRoute extends Route {
-  @service('browser/window') window;
-
   model() {
-    this.window.myGlobalVariable = 'Dos';
+    window.myGlobalVariable = 'Dos';
   }
 }
